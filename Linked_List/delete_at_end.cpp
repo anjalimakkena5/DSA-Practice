c4e4f1b31001390be03bac53a3825f2d2e24d7ca
Anjali <anjalimakkena.252cs018@nitk.edu.in>
2026-02-06 15:08:46 +0530

    deletion at end of linkedlist

---
+++ Linked_List/delete_at_end.cpp
@@ -0,0 +1,28 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* removeLastNode(Node* head) {
+        // code here
+        if(head==nullptr || head->next==nullptr) return nullptr;
+        Node* temp=head;
+        Node* prev=nullptr;
+        while(temp->next!=nullptr){
+            prev=temp;
+            temp=temp->next;
+        }
+        prev->next=nullptr;
+        delete temp;
+        return head;
+    }
+};
