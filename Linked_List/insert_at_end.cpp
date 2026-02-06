5b90f1773d8a3becfdd9f1ab36a10e962c6bf885
Anjali <anjalimakkena.252cs018@nitk.edu.in>
2026-02-06 12:47:28 +0530

    insertion at end of linkedlist

---
+++ Linked_List/insert_at_end.cpp
@@ -0,0 +1,33 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        // Code here
+        if(head==nullptr) return new Node(x);
+        Node* temp=head;
+        while(temp->next!=nullptr){
+            temp=temp->next;
+        }
+        Node* tail=new Node(x);
+        temp->next=tail;
+        return head;
+    }
+};
