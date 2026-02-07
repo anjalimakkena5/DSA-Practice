555a1014ce20df0cf6433ac39159fa6b63de65c0
Anjali <anjalimakkena.252cs018@nitk.edu.in>
2026-02-07 14:53:28 +0530

    two sum equal to target

---
+++ Arrays/two_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mpp;
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int rem=target-nums[i];
+            if(mpp.find(rem)!=mpp.end()){
+                return {mpp[rem],i};
+            }
+            mpp[nums[i]]=i;
+        }
+        return {-1,-1}; 
+    }
+};
